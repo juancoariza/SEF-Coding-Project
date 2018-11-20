8e48e592ab5fae9297be580cdaf38f3fece8ae71
juancoariza <35510324+juancoariza@users.noreply.github.com>
2018-11-20 07:53:52 -0500

    Update as of 11/20
    Needs input checking and GUI screen switch methods

--- sample/Program Logic/iCFAccountDataController.java
+++ sample/Program Logic/iCFAccountDataController.java
@@ -4,12 +4,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 public class iCFAccountDataController {
 
     @FXML
     private AnchorPane mainLoginScreen;
+
+    @FXML
+    private ImageView background;
 
     @FXML
     private Button createAcctButton;
@@ -21,6 +26,36 @@ public class iCFAccountDataController {
     private Label mainNameLabel;
 
     @FXML
+    private TextField nameCollector;
+
+    @FXML
+    private Label ageLabel;
+
+    @FXML
     private ChoiceBox<?> userAge;
+
+    @FXML
+    private ChoiceBox<?> userSex;
+
+    @FXML
+    private TextField usernameCollector;
+
+    @FXML
+    private Label sexLabel;
+
+    @FXML
+    private Label mainUsernameLabel;
+
+    @FXML
+    private Button cancelToLoginButton;
+
+    @FXML
+    private ImageView profilePic;
+
+    @FXML
+    private Label photoLabel;
+
+    @FXML
+    private Button imageChooserButton;
 
 }
