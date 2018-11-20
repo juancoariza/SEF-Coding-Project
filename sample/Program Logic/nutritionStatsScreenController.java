2822bf0250e08d4c7994908a690a709fda333759
juancoariza <35510324+juancoariza@users.noreply.github.com>
2018-11-20 07:58:40 -0500

    Update as of 11/20
    Added formatting for user value display and basic setup for a line graph (incomplete)

--- sample/Program Logic/nutritionStatsScreenController.java
+++ sample/Program Logic/nutritionStatsScreenController.java
@@ -1,11 +1,18 @@
 package sample.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
+import sample.testNutritionStatsDriver;
 
-public class nutritionStatsScreenController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class nutritionStatsScreenController implements Initializable {
+
+    testNutritionStatsDriver thisRun = new testNutritionStatsDriver();
 
     @FXML
     private LineChart<?, ?> weightLossLineGraph;
@@ -34,7 +41,21 @@ public class nutritionStatsScreenController {
     @FXML
     private Label lastWorkoutVal;
 
-    @FXML
-    private Label lastMealVal;
+    /* The method currently reads values from a driver class. This will be replaced with an User class */
 
+    public void generalLabelSettings() {
+
+        currWeightVal.setText(Double.toString(thisRun.getUserCurrWeight()));
+        currWeightGoalVal.setText(Double.toString(thisRun.getUserGoalWeight()));
+        lastWorkoutVal.setWrapText(true);
+        lastWorkoutVal.setText(thisRun.getUserLastWorkout());
+
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        generalLabelSettings();
+
+    }
 }
